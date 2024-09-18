15311308978640a3e9d0102718145fd11d337c10
JorgeRiveraMio <jorgeriveramio1008@gmail.com>
2024-09-17 21:19:22 -0500

    agregue el coors

--- src/main/java/com/example/CostenoBackend/Controller/ConsultaDni.java
+++ src/main/java/com/example/CostenoBackend/Controller/ConsultaDni.java
@@ -1,5 +1,6 @@
 package com.example.CostenoBackend.Controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ public class ConsultaDni {
     public ConsultaDni(ReniecService reniecService) {
         this.reniecService = reniecService;
     }
-
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/consultar-dni")
     public String consultarDNI(@RequestParam String dni) {
         return reniecService.consultarDNI(dni);
